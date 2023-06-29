0cf43c6f9eb762f47eeb2deb7ad9f6ccbf09f649
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-06-29 16:30:04 -0400

    Aula 5 – Variáveis e Tipos Primitivos

---
+++ Video em curso/Javascript/Aula 5 – Variáveis e Tipos Primitivos/comandos.js
@@ -0,0 +1,26 @@
+var nome = 'Matheus' // Define
+console.log(nome)
+nome = 'Paulo' // Substitue
+console.log(nome)
+
+var idade = 18
+var salário = 1550.54
+var sexo = 'M'
+
+console.log(`Idade: ${idade}\nSalário: ${salário}\nSexo: ${sexo}`)
+
+
+var n1 = 8
+let n2 = 5
+
+console.log('Valor 1: ' + n1)
+console.log('Valor 2: ' + n2)
+console.log(n1 + n2)
+console.log(n1 + " é um " + typeof n1)
+console.log(nome + " é uma " + typeof nome)
+console.log('"142" é uma ' + typeof "142")
+console.log('[] ou {} é um ' + typeof [])
+console.log('uma function é literalmente uma ' + typeof function(){})
+console.log('um undefined é literalmente um ' + typeof undefined)
+console.log('NaN ou Infinity é um ' + typeof NaN)
+console.log('null é um ' + typeof null)
