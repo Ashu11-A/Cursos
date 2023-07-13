892609f0045613bfdd9569f62cbc564a2cc129fa
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-13 15:57:08 -0400

    yeye

---
+++ Video em curso/Javascript/Aula 14 – Repetições (Parte 2)/exemplos.js
@@ -0,0 +1,17 @@
+/*
+var contador = 1
+while (contador <= 10) {
+    console.log('Exemplo1 ' + contador)
+    contador++
+}
+
+do {
+    console.log('Exemplo2 ' + contador)
+    contador++
+} while (contador <= 10)
+*/
+console.log('Que o caos reine...')
+for (var contador = 1; contador <= 10; contador++) {
+    console.log('passo ' + contador)
+}
+console.log('Apenas começou...')
