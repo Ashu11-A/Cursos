b1cb877c461df61516de8082bdbe058615529053
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-08-10 13:02:47 -0300

    Finality

---
+++ Rocketseat/TUDO que você deve estudar de JavaScript antes do React/(10) Named Export.js
@@ -0,0 +1,9 @@
+import {soma as somar, sub, PI} from './Modules/match'
+import sum from './Modules/sum'
+import * as match from './Modules/match'
+
+console.log(somar(1, 7)) // 8
+console.log(sub(3, 2)) // 1
+console.log(PI) // 3.14....
+console.log(sum(1, 6)) // 7
+console.log(match.sub(50, 25)) // 25
---
+++ Rocketseat/TUDO que você deve estudar de JavaScript antes do React/Modules/sum.js
@@ -0,0 +1,1 @@
+export{ soma as sum } from './match'
